926df1895b02ef789d30f9ee7ac3e48253a16de0
Duc Van Trang <trang.ducvan@icloud.com>
2019-09-19 14:05:48 +0200

    Rename scabble.py to scrabble_lib.py

--- test/scrabble_test.py
+++ test/scrabble_test.py
@@ -7,27 +7,27 @@ import sys
 sys.path.append("../src")
 sys.path.append("src")
 
-from scrabble import (count_points, find_best, generate_random_input,
-                      load_dictionary_file, load_distribution_file,
-                      sort_all_letters_by_points, sort_letters_by_points)
+import scrabble_lib
 
 DATA_DIR = "data"
 DICTIONARY_FILE = "francais.txt"
 DISTRIBUTION_FILE = "french_scrabble_distribution_with_points.csv"
 
 if __name__ == "__main__":
-    letter_set, letter_points = load_distribution_file("{}/{}".format(
-        DATA_DIR, DISTRIBUTION_FILE))
+    letter_set, letter_points = scrabble_lib.load_distribution_file(
+        "{}/{}".format(DATA_DIR, DISTRIBUTION_FILE))
 
     if not letter_set:
         exit(1)
 
-    letters = generate_random_input(letter_set)
+    letters = scrabble_lib.generate_random_input(letter_set)
     print(letters)
 
-    word_map = load_dictionary_file("{}/{}".format(DATA_DIR, DICTIONARY_FILE))
+    word_map = scrabble_lib.load_dictionary_file("{}/{}".format(
+        DATA_DIR, DICTIONARY_FILE))
 
-    best_words = find_best(letter_points, word_map, letters)
+    best_words = scrabble_lib.find_best(letter_points, word_map, letters)
     print(best_words)
     if best_words:
-        print("Points:", count_points(letter_points, best_words[0]))
+        print("Points:", scrabble_lib.count_points(letter_points,
+                                                   best_words[0]))
