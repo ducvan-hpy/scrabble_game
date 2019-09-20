763c067295685051835c8d9404e0f409cf4dbb87
Duc Van Trang <trang.ducvan@icloud.com>
2019-09-20 22:25:53 +0200

    Save game logs in a file

--- src/scrabble_game.py
+++ src/scrabble_game.py
@@ -12,10 +12,11 @@ SCRABBLE_POINTS = 50
 
 
 class Player:
-    def  __init__(self, name):
+    def  __init__(self, name, logfile):
         self._name = name
         self._letters = []
         self._points = 0
+        self._logfile = logfile
 
     def get_name(self):
         return self._name
@@ -44,8 +45,8 @@ class Player:
             except ValueError:
                 # Blank tile used as joker
                 self._letters.remove(scrabble_lib.BLANK_TILE_NAME)
-                log_action("Player {} used blank tile to replace '{}'".format(
-                    self.get_name(), l))
+                log_action(self._logfile, "Player {} used blank tile to "
+                           "replace '{}'".format(self.get_name(), l))
 
         return removed_letters
 
@@ -55,14 +56,21 @@ class Player:
 
 class Game:
     def __init__(self, dictionary_file, distribution_file):
+        # Format log file name: "game_YYYY_MM_DD_hh_mm_ss.log"
+        dt = datetime.now()
+        self.logfile = dt.strftime("game_%Y%m%d_%H%M%S.log")
+        print("Game logs will be saved in {}".format(self.logfile))
+
         self.letter_set, self.letter_points = \
             scrabble_lib.load_distribution_file(distribution_file)
         self.n_tiles = sum(self.letter_set.values())
-        log_action("Start game with {} tiles\n===".format(self.n_tiles))
         self.word_map = scrabble_lib.load_dictionary_file(dictionary_file)
 
+        log_action(self.logfile,
+                   "Start game with {} tiles\n===".format(self.n_tiles))
+
         # Named player "1", "2", "3"...
-        self.players = [Player(i + 1) for i in range(N_PLAYERS)]
+        self.players = [Player(i + 1, self.logfile) for i in range(N_PLAYERS)]
         self.player_turn = 0 # index of the player to play next
 
     def current_player(self):
@@ -95,7 +103,7 @@ class Game:
         while not l or l == scrabble_lib.BLANK_TILE_NAME:
             l = scrabble_lib.generate_random_input(letter_set, 1)[0]
             letter_set[l] -= 1
-            log_action("Player {} has picked a '{}' tile".
+            log_action(self.logfile, "Player {} has picked a '{}' tile".
                        format(player.get_name(), l))
 
         return l
@@ -106,7 +114,8 @@ class Game:
         letters = scrabble_lib.generate_random_input(self.letter_set, n_letters)
         self.remove_letters_from_set(letters)
         player.add_letters(letters)
-        log_action("Player {} has picked {}".format(player.get_name(), letters))
+        log_action(self.logfile,
+                   "Player {} has picked {}".format(player.get_name(), letters))
 
 
     def current_player_substitute_letters(self):
@@ -120,7 +129,7 @@ class Game:
                                                      scrabble_lib.SET_SIZE)
         old_letters = player.get_letters()
 
-        log_action("Player {} has swapped {} for {}".format(
+        log_action(self.logfile,"Player {} has swapped {} for {}".format(
             player.get_name(), old_letters, letters))
 
         self.remove_letters_from_set(letters)
@@ -143,7 +152,8 @@ class Game:
         self.player_turn = players_start_tile.index(min_tile)
 
         player = self.current_player()
-        log_action("Player {} has the tile closest to 'A', he will be the "
+        log_action(self.logfile,
+                   "Player {} has the tile closest to 'A', he will be the "
                    "first player to play\n===".format(player.get_name()))
 
         for _ in range(N_PLAYERS):
@@ -156,8 +166,8 @@ class Game:
         Return True if he can play, False otherwise.
         '''
         player = self.current_player()
-        log_action("--\nPlayer {} hand: {}".format(player.get_name(),
-                                                   player.get_letters()))
+        log_action(self.logfile, "--\nPlayer {} hand: {}"
+                   .format(player.get_name(), player.get_letters()))
 
         # Because of blank tiles, we will recompute the points
         best_word, _ = scrabble_lib.find_best(self.letter_points, self.word_map,
@@ -170,12 +180,13 @@ class Game:
 
             player.add_points(points)
 
-            log_action("Player {} played '{}' for {} points"
+            log_action(self.logfile, "Player {} played '{}' for {} points"
                        .format(player.get_name(), best_word, points))
 
             if len(best_word) == scrabble_lib.SET_SIZE:
                 player.add_points(SCRABBLE_POINTS)
-                log_action("Player {} played {} tiles and got {} extra points"
+                log_action(self.logfile,
+                           "Player {} played {} tiles and got {} extra points"
                            .format(player.get_name(), len(best_word),
                                    SCRABBLE_POINTS))
 
@@ -183,7 +194,8 @@ class Game:
                 self.current_player_pick_letters()
             return True
         else:
-            log_action("Player {} cannot play".format(player.get_name()))
+            log_action(self.logfile,
+                       "Player {} cannot play".format(player.get_name()))
             return False
 
     def end_game(self):
@@ -195,15 +207,15 @@ class Game:
                     can_play = True
                 self.next_player()
 
-        log_action("===")
+        log_action(self.logfile, "===")
 
         for _ in range(N_PLAYERS):
             player = self.current_player()
-            log_action("Player {} hand: {}".format(player.get_name(),
-                                                   player.get_letters()))
+            log_action(self.logfile, "Player {} hand: {}"
+                       .format(player.get_name(), player.get_letters()))
 
-            log_action("Player {} got {} points".format(player.get_name(),
-                                                        player.get_points()))
+            log_action(self.logfile, "Player {} got {} points"
+                       .format(player.get_name(), player.get_points()))
             self.next_player()
 
     def play(self):
@@ -217,10 +229,12 @@ class Game:
                 self.current_player_substitute_letters()
             self.next_player()
 
-        log_action("===\n{} tiles remaining. Finishing the game with tiles "
+        log_action(self.logfile,
+                   "===\n{} tiles remaining. Finishing the game with tiles "
                    "in players hands.".format(self.n_tiles))
 
         self.end_game()
 
-def log_action(message):
-        print(message)
+def log_action(filename, message):
+    with open(filename, "a") as f:
+        f.write(message + "\n")
